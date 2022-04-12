6fd7d671a7d7af96af2a5fba048bdf2cd7e5cb4d
Pirmin Pernsteiner <pirmin.pernsteiner@gmail.com>
2022-04-12 12:06:05 -0300

    Add task 1 of project

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,18 +1,75 @@
 import React from 'react';
+import Input from './Input';
 
 class Form extends React.Component {
   render() {
     return (
-      <Form>
-        <Input>
-          type="text"
-          name="cardName"
-          data-testid="name-input"
-          labe="Nome"
-        </Input>
-      </Form>
-    )
+      <div>
+        <h1>Adicoinar Nova Carta</h1>
+        <form>
+          <Input
+            type="text"
+            name="cardName"
+            testid="name-input"
+            label="Nome"
+          />
+          <Input
+            type="textarea"
+            name="cardDescription"
+            testid="description-input"
+            label="Descrição"
+          />
+          <Input
+            type="number"
+            name="cardAttr1"
+            testid="attr1-input"
+            label="Attr01"
+          />
+          <Input
+            type="number"
+            name="cardAttr2"
+            testid="attr2-input"
+            label="Attr02"
+          />
+          <Input
+            type="number"
+            name="cardAttr3"
+            testid="attr3-input"
+            label="Attr03"
+          />
+          <Input
+            type="text"
+            name="cardImage"
+            testid="image-input"
+            label="Imagem"
+          />
+          <label htmlFor="cardRare">
+            Raridade
+            <select
+              name="cardRare"
+              data-testid="rare-input"
+            >
+              <option value="normal">normal</option>
+              <option value="raro">raro</option>
+              <option value="muito raro">muito raro</option>
+            </select>
+          </label>
+          <Input
+            type="checkbox"
+            name="cardTrunfo"
+            testid="trunfo-input"
+            label="Super Trybe Trunfo"
+          />
+          <button
+            data-testid="save-button"
+            type="submit"
+          >
+            Salvar
+          </button>
+        </form>
+      </div>
+    );
   }
- 
 }
+
 export default Form;
--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,1 +1,27 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+
+class Input extends React.Component {
+  render() {
+    const { type, name, testid, label } = this.props;
+    return (
+      <label htmlFor={ name }>
+        { label }
+        <input
+          data-testid={ testid }
+          type={ type }
+          name={ name }
+        />
+      </label>
+    );
+  }
+}
+
+Input.propTypes = {
+  type: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  testid: PropTypes.string.isRequired,
+  label: PropTypes.string.isRequired,
+};
+
+export default Input;
